1159acd6c0ddd4eb9f7ec0e4f476ba8a2f704633
Henrique Morbin <morbin_@hotmail.com>
2016-05-21 17:16:52 -0300

    Initial Commit

---
+++ FuelBattle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FuelBattle
+//
+//  Created by Henrique Morbin on 21/05/16.
+//  Copyright © 2016 Henrique Morbin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
